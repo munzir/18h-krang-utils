3c4523429885c4c78b2f5b7de5dcf5b619dd94b5
apatel435 <apatel435@gatech.edu>
2018-09-22 13:34:04 -0400

    Removed the readCosts function:

--- adrc.hpp
+++ adrc.hpp
@@ -12,9 +12,6 @@
 using namespace dart;
 using namespace dart::dynamics;
 
-// read the costs Q and R
-void readCosts(Eigen::MatrixXd& Q, Eigen::MatrixXd& R);
-
 // get body com
 Eigen::Vector3d getBodyCOM(SkeletonPtr robot);
 
@@ -23,11 +20,11 @@ void computeLinearizedDynamics(const SkeletonPtr robot, \
   Eigen::MatrixXd& A, Eigen::MatrixXd& B, Eigen::VectorXd& B_thWheel, Eigen::VectorXd& B_thCOM);
 
 // perform ADRC
-void activeDisturbanceRejectionControl( 
+void activeDisturbanceRejectionControl(
   //inputs
-  const Eigen::MatrixXd& A, const Eigen::MatrixXd& B, const Eigen::MatrixXd& Q, const Eigen::MatrixXd& R, 
-  ESO* EthWheel, ESO* EthCOM, const Eigen::VectorXd& B_thWheel, const Eigen::VectorXd& B_thCOM, 
-  const Eigen::VectorXd& state, const Eigen::VectorXd& refState, //refState is (thCOM, dthCOM, thWheels, dthWheels, thSpin, dthSpin) 
+  const Eigen::MatrixXd& A, const Eigen::MatrixXd& B, const Eigen::MatrixXd& Q, const Eigen::MatrixXd& R,
+  ESO* EthWheel, ESO* EthCOM, const Eigen::VectorXd& B_thWheel, const Eigen::VectorXd& B_thCOM,
+  const Eigen::VectorXd& state, const Eigen::VectorXd& refState, //refState is (thCOM, dthCOM, thWheels, dthWheels, thSpin, dthSpin)
   const double& dt,
   // outputs
   Eigen::VectorXd& u_thWheel, Eigen::VectorXd& u_thCOM);

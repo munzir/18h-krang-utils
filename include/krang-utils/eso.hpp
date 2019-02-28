6b66b8494c2a08232e43329ff35a5e8233e1c9c9
Munzir Zafar <mzafar7@gatech.edu>
2019-02-28 13:08:00 -0500

    Added #define at the start of Eso.hpp

--- include/krang-utils/eso.hpp
+++ include/krang-utils/eso.hpp
@@ -1,22 +1,28 @@
 #include <Eigen/Eigen>
 
+#ifndef KRANG_UTILS__ESO_HPP_
+#define KRANG_UTILS__ESO_HPP_
 
 class Eso {
-public:
+ public:
   Eso(const Eigen::VectorXd& X0, const Eigen::VectorXd& L_) : mL(L_), mX(X0) {
-  	int n = X0.size();
-  	mA = Eigen::MatrixXd::Zero(n, n);
-  	for(int i=0; i<n-1; i++) mA(i, i+1) = 1;
+    int n = X0.size();
+    mA = Eigen::MatrixXd::Zero(n, n);
+    for (int i = 0; i < n - 1; i++) mA(i, i + 1) = 1;
   }
 
-  Eigen::VectorXd update(const double& Z, const Eigen::VectorXd& B, const Eigen::VectorXd& u, const double& dt) {
-  	mX += (mA*mX + mL*(Z - mX(0)) + B*u)*dt;
-  	return mX;
+  Eigen::VectorXd update(const double& Z, const Eigen::VectorXd& B,
+                         const Eigen::VectorXd& u, const double& dt) {
+    mX += (mA * mX + mL * (Z - mX(0)) + B * u) * dt;
+    return mX;
   }
 
   Eigen::VectorXd getState() { return mX; }
-private:
+
+ private:
   Eigen::VectorXd mL;
   Eigen::VectorXd mX;
   Eigen::MatrixXd mA;
 };
+
+#endif // KRANG_UTILS__ESO_HPP_
